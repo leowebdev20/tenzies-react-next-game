1aa5b22715a7c38e29ba4539455198e57f2068a5
leowebdev20 <serlebeiso@gmail.com>
2023-08-18 21:23:53 +0200

    dice component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
+'use client';
+import { useState } from "react";
 import Image from "next/image";
+import Die from "./Die";
 
 export default function Home() {
-  return <main></main>;
+  const [dice, setDice] = useState(allNewDice());
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
+    }
+    return newDice;
+  }
+
+  const diceElements = dice.map((die) => <Die key={die} value={die} />);
+
+  return (
+    <main>
+      <div className="dice-container">{diceElements}</div>
+    </main>
+  );
 }
